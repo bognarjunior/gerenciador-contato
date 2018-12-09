b1c3623464534435d9011691456e15078a0e065f
Bognar Junior <bognar_junior@yahoo.com.br>
2018-12-08 23:12:49 -0200

    Criando estrutura básica
    Criando a estrutura básica do gerenciador de contatos

---
+++ src/components/Cadastro.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Listar extends Component {
+    
+  render() {
+    return (<h1>Cadastro de contatos</h1>)
+  }
+}
---
+++ src/components/Listar.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+export default class Listar extends Component {
+    
+  render() {
+    return (<h1>Lista de contatos</h1>)
+  }
+}
